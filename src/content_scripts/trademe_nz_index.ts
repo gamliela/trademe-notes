3027f60c09212d83e2fbbd9e2ef87f299243a34b
Alon Gamliel <alon@gamliela.com>
2019-03-10 18:08:51 +1300

    Added Bulma and FontAwesome to the project

--- src/content_scripts/trademe_nz_index.ts
+++ src/content_scripts/trademe_nz_index.ts
@@ -1,3 +1,5 @@
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bulma/css/bulma.css';
 import {installWrappingKeyboardTrigger} from "./wrap_service";
 
 installWrappingKeyboardTrigger();
